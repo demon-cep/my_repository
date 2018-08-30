349bcbcbb80490f0d914f55fc83f7a5b731b2b98
Serega <demon-cep@yandex.ru>
2018-08-30 16:42:46 +0700

    modals_popap finish

---
+++ module/popap_modals/include_futer.php
@@ -0,0 +1,27 @@
+    <div id="overlay" style="display: none;"></div>
+            <div id="callback-window" class="modal" style="display: none;"> <a class="modal-close" href="#"></a>
+                <div class="spinner"></div>
+                <div class="modal-content">
+                    <h2><span>Обратный звонок</span></h2>
+                    <div class="">
+                    <form action="" method="post" data-file="callback">
+                        <input type="text" name="name" placeholder="Ваше имя *">
+                        <input type="text" name="phone" placeholder="Номер телефона *">
+                        <input type="text" name="time" placeholder="Удобное время для звонка">
+                        <textarea name="text" placeholder="Здесь вы можете оставить комментарий"></textarea>
+                        <div class="modal-controls">
+                            <button type="submit" class="bigbutt btn btn_primary-color">Отправить</button>
+                        </div>
+                        <div class="modal-errors">
+                        </div>
+                        <div class="modal-result">
+                        </div>
+                    </form>
+                    </div>
+                </div>
+                <div class="modal-content-copy"></div>
+            </div>
+
+    <script type="text/javascript" src="/js/jquery.form.min.js"></script>
+        <!-- всплывашка для добовления товара -->
+        <script type="text/javascript" src="/js/modals.js"></script>
